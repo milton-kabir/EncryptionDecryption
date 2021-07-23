3b0c4d1caf474d96f6f5b42f8b343c29dc6c2d13
milton-kabir <milton.cse.pust@gmail.com>
2021-07-23 10:41:07 +0600

    Fourth Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -7,27 +7,35 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         Scanner scanner = new Scanner(System.in);
-        String op = scanner.nextLine();
-        String st = scanner.nextLine();
+        String op = "enc";
+        String st = "";
 //        String aa = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
-        int ck = scanner.nextInt();
+        int ck = 0;
+        for (int i = 0; i < args.length; i += 2) {
+            if (args[i].matches("-mode")) {
+                op = args[i + 1];
+            } else if (args[i].matches("-data")) {
+                st = args[i + 1];
+            } else if (args[i].matches("-key")) {
+                ck = Integer.parseInt(args[i + 1]);
+            }
+        }
+
         if (op.matches("enc")) {
             for (int i = 0; i < st.length(); i++) {
                 int x = st.charAt(i);
                 x += ck;
-                char ch = (char)(x);
+                char ch = (char) (x);
                 st = st.substring(0, i) + ch + st.substring(i + 1);
             }
-        }
-        else{
+        } else {
             for (int i = 0; i < st.length(); i++) {
                 int x = st.charAt(i);
                 x -= ck;
-                char ch = (char)(x);
+                char ch = (char) (x);
                 st = st.substring(0, i) + ch + st.substring(i + 1);
             }
         }
-
         System.out.println(st);
     }
 }
