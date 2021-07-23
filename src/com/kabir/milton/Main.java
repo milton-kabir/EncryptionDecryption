05504cfe303f6f4ab8a31c121951b740471f8f67
milton-kabir <milton.cse.pust@gmail.com>
2021-07-23 09:55:32 +0600

    First Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -6,14 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-//        Scanner scanner=new Scanner(System.in);
         String st="we found a treasure!";
         for(int i=0;i<st.length();i++){
             int x=st.charAt(i);
-//            System.out.print(x+"->");
             x=97+25-x+97;
             char ch=(char)x;
-//            System.out.println(x);
             if(st.charAt(i)>='a'&&st.charAt(i)<='z') {
                 st = st.substring(0, i) + ch + st.substring(i + 1);
             }
