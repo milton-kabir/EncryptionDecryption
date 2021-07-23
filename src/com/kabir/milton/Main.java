e34723096e402be32ce4758b7b82b0c5a3b0cb18
milton-kabir <milton.cse.pust@gmail.com>
2021-07-23 10:15:18 +0600

    Third Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -5,19 +5,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Scanner scanner=new Scanner(System.in);
-        String st=scanner.nextLine();
-        String aa="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
-        int ck=scanner.nextInt();
-        for(int i=0;i<st.length();i++){
-            if(st.charAt(i)>='a'&&st.charAt(i)<='z') {
-                int x=st.charAt(i)-97;
-                x+=ck;
-                char ch=aa.charAt(x);
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        String op = scanner.nextLine();
+        String st = scanner.nextLine();
+//        String aa = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+        int ck = scanner.nextInt();
+        if (op.matches("enc")) {
+            for (int i = 0; i < st.length(); i++) {
+                int x = st.charAt(i);
+                x += ck;
+                char ch = (char)(x);
                 st = st.substring(0, i) + ch + st.substring(i + 1);
             }
         }
+        else{
+            for (int i = 0; i < st.length(); i++) {
+                int x = st.charAt(i);
+                x -= ck;
+                char ch = (char)(x);
+                st = st.substring(0, i) + ch + st.substring(i + 1);
+            }
+        }
+
         System.out.println(st);
     }
 }
